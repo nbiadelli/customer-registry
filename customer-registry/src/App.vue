982e6564e6277d0fac45c670a735f89fb0690ece
Natalia Biadelli <natalia.biadelli@gmail.com>
2022-12-08 10:38:59 -0300

    login views were created

--- customer-registry/src/App.vue
+++ customer-registry/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
@@ -10,15 +8,11 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: $primitives-purple-3;
+  font-family: "Nunito Sans", sans-serif;
 }
 
 #nav {
-  padding: 30px;
 
   a {
     font-weight: bold;
---
+++ customer-registry/src/views/Login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="c-login">
+    <h2 class="c-login__title">Login</h2>
+    <form action="" class="c-login__form">
+      <div class="c-login__user-field">
+        <input-text 
+          :label="'Usuario'"
+          id="user" 
+          for="user" 
+          type="text" 
+        />
+      </div>
+      <div class="c-login__user-field">
+        <input-text
+          :label="'Password'"
+          id="password"
+          for="password"
+          type="password"
+        />
+      </div>
+      <div class="c-login__footer">
+        <a href="#" id="submit" class="c-login__submit">Login</a>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="scss">
+body {
+  background-color: $primitives-gray-5;
+  font-family: "Nunito Sans", sans-serif;
+}
+
+.c-login {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  border-radius: 12px;
+  background-color: $primitives-blue-1;
+  transform: translate(-50%, -50%);
+
+  .c-login__title {
+    margin: 0 0 30px;
+    padding: 0;
+    color: $primitives-purple-10;
+    text-align: center;
+  }
+
+  .c-login__footer {
+    display: flex;
+    margin-top: 20px;
+
+    .c-login__submit {
+      margin: auto;
+      padding: 10px 20px;
+      color: $primitives-purple-10;
+      font-size: 16px;
+      text-decoration: none;
+      text-transform: uppercase;
+      letter-spacing: 4px;
+      border: 1px solid $primitives-purple-10;
+      overflow: hidden;
+      transition: 0.5s;
+
+      &:hover {
+        color: $primitives-purple-10;
+        border-radius: 5px;
+        background-color: $primitives-purple-5;
+        box-shadow: 0 0 5px $primitives-purple-5, 0 0 25px $primitives-purple-5,
+          0 0 50px $primitives-purple-5, 0 0 100px $primitives-purple-5;
+      }
+    }
+
+    .c-login__register {
+      margin: auto;
+      width: 60%;
+      font-size: 14px;
+      text-align: center;
+      text-decoration: none;
+    }
+  }
+}
+</style>
